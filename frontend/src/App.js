965a3ead4af9d79364752e2bbe662acb7b72c71b
Anjoom0901@ <nuranjoom@gmail.com>
2023-10-09 20:58:54 +0600

    Basic Routes are implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from 'C:/MiniRealEstate/MiniRealEstate/frontend/src/containers/Home.js';
+import Login from 'C:/MiniRealEstate/MiniRealEstate/frontend/src/containers/Login.js';
+import SignUp from 'C:/MiniRealEstate/MiniRealEstate/frontend/src/containers/SignUp.js';
+import Activate from 'C:/MiniRealEstate/MiniRealEstate/frontend/src/containers/Activate.js';
+import ResetPassword from 'C:/MiniRealEstate/MiniRealEstate/frontend/src/containers/ResetPassword';
+import ResetPasswordConfirm from 'C:/MiniRealEstate/MiniRealEstate/frontend/src/containers/ResetPasswordConfirm';
+//import Facebook from './containers/Facebook';
+//import Google from './containers/Google';
+
+//import { Provider } from 'react-redux';
+//import store from './store';
+
+import Layout from 'C:/MiniRealEstate/MiniRealEstate/frontend/src/hocs/Layout';
+
+const App = () => (  
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route exact path='/' element={<Home/>} />
+                    <Route exact path='/login' element={<Login/>} />
+                    <Route exact path='/signup' element={<SignUp/>} />
+                    <Route exact path='/reset-password' element={<ResetPassword/>} />
+                    <Route exact path='/password/reset/confirm/:uid/:token' component={<ResetPasswordConfirm/>} />
+                    <Route exact path='/activate/:uid/:token' element={<Activate></Activate>} />
+                </Routes>
+            </Layout>
+        </Router>
+);
 
 export default App;
